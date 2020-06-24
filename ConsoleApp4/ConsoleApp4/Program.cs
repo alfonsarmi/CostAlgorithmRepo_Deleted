398e62cd58b79caab3991e97fbe7a16e284704b0
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2020-06-24 13:18:11 +0200

    BAck to intial status

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -10,27 +10,74 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
-            //This is a new comment
-            var a = 1;
-            if (a == 1)
-            {
-                switch (a)
-                {
-                    case 1:
-                        var aa = 1;
-                        Test();
-                        break;
-                    case 2:
-                        var b = 2;
-                        break;
+			var classs = new Class1();
+			classs.Caca1();
 
-                    default:
-                        //do nothing
-                        break;
-                }
-            }			
+			var a = 1;
+			var b = 1;
+			while (a == 2)
+			{
+				if (a == 2)
+				{
+					//this is a test 1
+					var v = 1;
 
-		}
+					if (a == 2)
+					{
+						//this is a test 2
+						var tv1 = 1;
+
+						if (a == 2)
+						{
+							//this is a test 2
+							var tv2 = 1;
+
+							if (a == 2)
+							{
+								//this is a test 2
+								var tv3 = 1;
+							}
+						}
+					}
+				}
+
+				if (a == 2)
+				{
+					//this is a test 3
+					var oov = 1;
+				}
+			}
+
+			if (a == 1)
+			{
+				if (a == 1 && b == 2 || b == 1)
+				{
+					if (b == 2)
+					{
+						var p = 1;
+						switch (b)
+						{
+							case 1:
+								var z = 1;
+								break;
+
+							case 2:
+								var w = 2;
+								break;
+
+							default:
+								var o = 3;
+								break;
+
+						}
+
+					}
+				}
+
+			}
+		
+
+	}
 
         private static void Test()
         {
